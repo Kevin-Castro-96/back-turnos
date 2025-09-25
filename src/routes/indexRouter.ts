43a9b7167ef9a87db8e9d0e55c2066f62df3ad45
kevin-castro-96 <street123castle@gmail.com>
2025-09-25 15:13:29 -0300

    mensaje en inicio

--- src/routes/indexRouter.ts
+++ src/routes/indexRouter.ts
@@ -6,5 +6,9 @@ const indexRouter: Router = Router();
 
 indexRouter.use("/users", userRouter);
 indexRouter.use("/appointments", turnosRouter)
+indexRouter.use("/", (req, res) => {
+  res.send("🚀 Bienvenido a mi backend, la API está corriendo correctamente!");
+})
+
 
 export default indexRouter;
